9cab4bfad289414f7f2ed25ceb9c3c282470978a
yurttav <35852138+yurttav@users.noreply.github.com>
2020-06-28 04:21:02 +0300

    bot + settings

--- bot.js
+++ bot.js
@@ -7,35 +7,23 @@ const sqlite3 = require('sqlite3').verbose();
 var prefix = settings.prefix;
 
 //Purge function
-
 async function purge(message, args) {
-  //message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete. 
 
   const fetched = await message.channel.messages.fetch({limit: args}); // This grabs the last number(args) of messages in the channel.
   console.log(fetched.size + ' messages found, deleting...'); // Lets post into console how many messages we are deleting
   let msgcount = fetched.size;
   // Deleting the messages
   message.channel.bulkDelete(fetched)
-  /*
-      .then(msg => {
-        //message.channel.send(fetched.size + ' tane mesaj sildim!');
-        //msg.delete({ timeout: 5000 });
-        //console.log(msg);
-        console.log("silme başarılı");              
-      })
-      */
-      .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.   
-  console.log("Purge içinde msgcount: ",msgcount);
+    .catch(error => message.channel.send(`Aysnc Function Globar Purge Error: ${error}`)); // If it finds an error, it posts it into the channel.   
+  //console.log("Purge içinde msgcount: ",msgcount);
   return msgcount;
 }
-
 
 // Listener Event: Runs whenever a message is received.
 bot.on('message', message => {
 
-    // Variables - Variables make it easy to call things, since it requires less typing.
-    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
-    let sender = message.author.id; // This variable takes the message, and finds who the author is.
+    let msg = message.content.toUpperCase(); 
+    let sender = message.author.id; 
     let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
     let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
     let isgunlukkanal = false;
@@ -50,29 +38,19 @@ bot.on('message', message => {
       case 'S.A.': message.reply('AS bro!').then(d_msg => { d_msg.delete({ timeout: 10000 });}); break;
       case 'SAE': if (isgunlukkanal) {message.channel.send('Dikkat et QR lı değilse blind olabilir!').then(d_msg => { d_msg.delete({ timeout: 10000 });});} break;
       case 'NTA': if (isgunlukkanal) {message.channel.send('Tüh be. Bitti mi <@'+sender+'>? Nasip işte!').then(d_msg => { d_msg.delete({ timeout: 10000 });});} break;
-      /*
-        if (isgunlukkanal) {
-          if (sender === settings.soykan){
-            message.channel.send('Hadi <@'+sender+'> Derviş göster kendini iste de gelsin!');
-          } else {
-            message.channel.send('Tüh be. Bitti mi <@'+sender+'> Nasip işte! İste de gelsin diyeceğim ama <@'+settings.soykan+'> Dervişe has bir durum! Ama yine de iste sen');
-          }
-        } 
-        break;
-      */  
       case 'ACF': if (isgunlukkanal) {message.channel.send('Kral geldi Kral!!! :)').then(d_msg => { d_msg.delete({ timeout: 10000 });});} break;
       case 'ALPACA': if (isgunlukkanal) {message.channel.send(`Hadi yine iyisiniz. <@${settings.frk}> <@${settings.desire}> size yok boşuna beklemeyin...!`).then(d_msg => { d_msg.delete({ timeout: 10000 });});} break;
       case 'DAYILAR': message.channel.send('Dayım benim! Nasılsın?').then(d_msg => { d_msg.delete({ timeout: 10000 });}); break;
-      case prefix + 'ADAM': message.channel.send(`<@${settings.sahip}>`).then(d_msg => { d_msg.delete({ timeout: 10000 });}); break;
+      case prefix + 'MASTER': message.channel.send(`<@${settings.sahip}>`).then(d_msg => { d_msg.delete({ timeout: 10000 });}); break;
       case prefix + 'DOWN': return; 
-    // Ping
-      case prefix + 'PING': message.channel.send('Ping!'); break;// This 'sends' the message to the channel the message was in. You can change what is in the message to whatever you want.
       case prefix + '?': message.channel.send(settings.help); break;
     }
 
     if ((msg === 'INŞ') || (msg === 'INS') || msg.includes(' INŞ ') || msg.includes(' INS ') || msg.includes('INŞ ') || msg.includes('INS ') || msg.includes('INSALLAH') || msg.includes('INSAALLAH') || msg.includes('INŞALLAH') || msg.includes('INŞALLAH')) {
       message.channel.send('Amin!').then(d_msg => { d_msg.delete({ timeout: 10000 });});
-    } else if (msg.includes('AEO') || msg.includes('GÖRÜŞÜRÜZ')) {
+    }  
+    
+    if (msg.includes('AEO') || msg.includes('GÖRÜŞÜRÜZ')) {
       message.channel.send('A.E.O. kardeşim!').then(d_msg => { d_msg.delete({ timeout: 10000 });});
     }
 
@@ -83,18 +61,17 @@ bot.on('message', message => {
         return;
       }
 
-      if (args[1] === null) {
-        message.channel.send('Bro! tek seferde azami 100 tane silmeme izin veriyor DISCORD. Kusura kalma. Bi zahmet yeniden dene!'); //\n means new line.
-        return;
-      }
+      botmsg = args.slice(1); 
+
+      botmsg = botmsg.toString();
       
-      args[1] = args[1].replace(/:/g, " ");
+      botmsg = botmsg.replace(/,/g, " ");
    
-      bot.channels.cache.get(args[0]).send(args[1]);
+      bot.channels.cache.get(args[0]).send(botmsg);
     } 
 
     // Purge
-    if (msg.startsWith(prefix + 'SİL') || msg.startsWith(prefix + 'SIL'))  { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+    if (msg.startsWith(prefix + 'XSİL') || msg.startsWith(prefix + 'XSIL'))  { // This time we have to use startsWith, since we will be adding a number to the end of the command.
     
     // We have to wrap this in an async since awaits only work in them.
       async function xpurge() {
@@ -134,26 +111,29 @@ bot.on('message', message => {
       xpurge(); // Make sure this is inside the if(msg.startsWith)
     }
 
-    if (msg.startsWith(prefix + 'XSİL') || msg.startsWith(prefix + 'XSIL'))  { // This time we have to use startsWith, since we will be adding a number to the end of the command.        
-        // We want to make sure we call the function whenever the purge command is run.
+    if (msg.startsWith(prefix + 'SİL') || msg.startsWith(prefix + 'SIL'))  { 
+      
+      message.delete();
+      
       if (isNaN(args[0])) {
-        message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'SİL 100'); //\n means new line.
+        message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'SİL 100'); 
         return;
       } 
 
       async function xsil() {     
-        let msgcount = await purge(message, args[0]); // Make sure this is inside the if(msg.startsWith)
-        console.log("Purge dışında msgcount: ",msgcount);
+        let msgcount = await purge(message, args[0]); 
+        //console.log("Purge dışında msgcount: ",msgcount);
         message.channel.send(msgcount + ' tane mesaj sildim!').then(d_msg => { d_msg.delete({ timeout: 5000 });});
       }
       xsil();
     }
-  
 
-    if (msg.startsWith(prefix + 'TOPTANSİL') || msg.startsWith(prefix + 'TOPTANSIL'))  { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+    if (msg.startsWith(prefix + 'TOPTANSİL') || msg.startsWith(prefix + 'TOPTANSIL'))  { 
     
+      message.delete();
+
       if (isNaN(args[0])) {
-        message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'SİL 100'); //\n means new line.
+        message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'TOPTANSİL 100'); 
         return;
       }
 
@@ -167,23 +147,25 @@ bot.on('message', message => {
           var i;
           for (i=0; i<quotient; i++){
             msgcount += await purge(message, 100)
-              .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.   ;
+              .catch(error => message.channel.send(`ToptanSil fonksiyonunda purge 100 komutunda Hata: ${error}`)); // If it finds an error, it posts it into the channel.   ;
           }
           if (remainder !== 0) {
           msgcount += await purge(message, remainder)
-            .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.  
+            .catch(error => message.channel.send(`ToptanSil fonksiyonunda purge kalan komutunda Hata: ${error}`)); // If it finds an error, it posts it into the channel.  
           }      
         } else
           msgcount += await purge(message, args[0]) 
-            .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.   
+            .catch(error => message.channel.send(`ToptanSil fonksiyonunda purge args[0] komutunda Hata: ${error}`)); // If it finds an error, it posts it into the channel.   
   
-        console.log("Purge dışında msgcount: ",msgcount);
+        //console.log("Purge dışında msgcount: ",msgcount);
         message.channel.send(msgcount + ' tane mesaj sildim!').then(d_msg => { d_msg.delete({ timeout: 5000 });});       
       }
       toptansil();
     }
 
     if (msg.startsWith(prefix + 'SPAM')) {
+
+      message.delete();
 
       if (isNaN(args[0])) {
         message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'SPAM 100'); 
@@ -206,23 +188,12 @@ bot.on('message', message => {
     }
 
     if (msg.startsWith(prefix + 'AYARLAR')) {
-      /*
-      message.guild.channels.cache.forEach(channel => {
-        if ((channel.type !== "category") & (channel.parentID !== '645025507041083406'))         
-          console.log(channel.id, channel.name, channel.type, channel.parentID, bot.channels.cache.get(channel.parentID).name);       
-      });
-      */
-     if (args[0] === 'undefined') {
+
+      if (args[0] === 'undefined') {
        console.log(`settings deneme = ${settings.deneme}`);
        return;
-     }
-     /*
-     if (isNaN(args[0])) {
-      message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'SİL 100'); //\n means new line.
-      console.log(`settings deneme = ${settings.deneme}`);
-      return;
       }
-      */
+      
       let key = args[0];
       console.log(`Önce : ${settings[key]}`);
       settings[key] = args[1];
@@ -231,11 +202,134 @@ bot.on('message', message => {
 
     if (msg.startsWith(prefix + 'KANALLAR')) {    
       message.guild.channels.cache.forEach(channel => {
-        if (channel.type !== "category")
+        if (channel.type !== "category"){
           console.log(channel.id, channel.name, channel.type, channel.parentID, bot.channels.cache.get(channel.parentID).name);
+          message.channel.send(bot.channels.cache.get(channel.parentID).name + " altında " + channel.name);
+        }
       });
     }
 
+    if (msg.startsWith(prefix + 'MESAJLAR')) {
+      message.delete();    
+      message.channel.messages.fetch({ limit: 100 })
+        .then(msgs => {
+          console.log("Toplam Mesaj Sayısı = ", msgs.size)
+          msgs.forEach(m => console.log(message.channel.id, message.channel.name, m.content));
+      });
+    }
+
+    if (msg.startsWith(prefix + 'ZZZ')) {
+      message.delete();    
+      
+      message.channel.messages.fetch().then(async messages => {
+        console.log(`${messages.size} procuradas.`);
+
+        let finalArray = [];
+
+        const putInArray = async (data) => finalArray.push(data);
+        const handleTime = (timestamp) => moment(timestamp).format("DD/MM/YYYY - hh:mm:ss a").replace("pm", "PM").reaplce("am", "AM"); 
+
+        for (const message of messages.array().reverse()) await putInArray(`${handleTime(message.timestamp)} ${msg.author.username} : ${msg.content}`); 
+
+        console.log(finalArray);
+        console.log(finalArray.length);
+      });
+    } 
+
+    if (msg.startsWith(prefix + 'NSİL') || msg.startsWith(prefix + 'NSIL'))  { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+      message.delete();   
+      
+      if (isNaN(args[0])) {
+        message.channel.send('Kaç tane bro! \n \nKullanım Şekli: ' + prefix + 'NSİL 100'); //\n means new line.
+        return;
+      }
+
+      async function nsil(number) {
+
+        message.channel.messages.fetch({ limit: number })
+          .then( async msgs => {
+            //console.log("Toplam Mesaj Sayısı = ", msgs.size)
+            let deletedmsgcount = msgs.size;
+            msgs.forEach(async m => {
+              temp = bot.channels.cache.get(message.channel.id).messages.cache.get(m.id);
+              console.log(temp.content);
+              temp.delete();
+            });
+            console.log(deletedmsgcount, " messages were deleted");
+            return deletedmsgcount;
+        });
+      }
+      
+      async function ntoptansil() {
+
+        let msgcount = 0;
+  
+        if (args[0] > 100) {
+          var quotient = Math.floor(args[0]/100);
+          var remainder = args[0] % 100;
+          var i;
+          for (i=0; i<quotient; i++){
+            msgcount += await nsil(100)
+              .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.   ;
+          }
+          if (remainder !== 0) {
+          msgcount += await nsil(remainder)
+            .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.  
+          }      
+        } else
+          msgcount += await nsil(args[0]) 
+            .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.   
+  
+        //console.log("Purge dışında msgcount: ",msgcount);
+        //message.channel.send(msgcount + ' tane mesaj sildim!').then(d_msg => { d_msg.delete({ timeout: 5000 });});       
+      }
+      ntoptansil();
+    }
+
+    /*
+    if (msg.startsWith(prefix + 'DELETE')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+      message.delete();   
+      
+      async function delfetch(number) {
+
+        message.channel.messages.fetch({ limit: number })
+          .then( async msgs => {
+            return msgs.size;
+          });
+      }
+
+      async function delsil(number) {
+
+        message.channel.messages.fetch({ limit: number })
+          .then( async msgs => {
+            //console.log("Toplam Mesaj Sayısı = ", msgs.size)
+            let deletedmsgcount = msgs.size;
+            msgs.forEach(async m => {
+              temp = bot.channels.cache.get(message.channel.id).messages.cache.get(m.id);
+              console.log(temp.content);
+              temp.delete();
+            });
+            console.log(deletedmsgcount, " messages were deleted");
+        });
+      }
+      
+      async function deltoptansil() {
+
+        let msgcount = 0;
+
+        let tobedeleted = await delfetch(100);
+
+        console.log(tobedeleted, " are ready");
+
+        while (tobedeleted > 0) {
+          delsil(100);
+          tobedeleted = await delfetch(100);
+          console.log(tobeleted, " are ready in while");
+        }
+      }
+      deltoptansil();
+    }
+   */
 });
 
 bot.on('channelCreate', channel => {
@@ -252,14 +346,12 @@ bot.on('presenceUpdate', (oldPresence, newPresence) => {
   let username = newPresence.user.username;
 
   if (newPresence.user.bot) {
-    console.log(`Bot statüsü değişti: ${username} \t\t${status}`);
+    //console.log(`Bot statüsü değişti: ${username} \t\t${status}`);
     return;
   } 
 
   if (typeof oldPresence === 'undefined') {
-    console.log(`Tanımsız Presence`);
-    console.log(username, status);
-    //console.log(newPresence.user.client.guilds);
+    console.log(`Tanımsız Presence\t\t ${username} \t ${status}`);
     var oldstatus = 'offline';
   } else {
     var oldstatus = oldPresence.status;
@@ -270,31 +362,12 @@ bot.on('presenceUpdate', (oldPresence, newPresence) => {
     //console.log(`Kanal = ${channel.id}\t ${channel.name} channel`);
     if ((channel.name === "genel")  || (channel.name === "günlük") || (channel.name === "gunluk")) salutechid = channel.id;
   });
-  //silinecek sonra
-  //salutechid = settings.gunluk;
-
+  
   if (salutechid == 0) {
     console.log("Salute Kanalı Tanımlı Değil...");
     return;
   }
-  /*
-  switch (g_id) {
-    case settings.home_office: salutech = settings.gunluk; break;
-    case '722044772750983238': salutech = '722044772750983241'; break;
-  }
-  */
-  /*
-  console.log(`Guild = ${g_id}\t ${newPresence.guild.name}\t\tKanalları`);
-
-  newPresence.guild.channels.cache.forEach((channel) => {
-    console.log(`Kanal = ${channel.id}\t ${channel.name} channel`);
-  });
-  */
-
-  //hangi guild bu
-  //if (g_id !== settings.home_office) return;
-
-  //let gunluk = bot.channels.cache.get(settings.gunluk);
+  
   let salutech = bot.channels.cache.get(salutechid);
 
   //db bağlan
@@ -305,7 +378,7 @@ bot.on('presenceUpdate', (oldPresence, newPresence) => {
       //db.run(`CREATE TABLE IF NOT EXISTS users (UserID NUMERIC UNIQUE, UserName TEXT, LastSalute	TEXT)`);
       //console.log("DB yok. Yeni DB oluşturuldu")
     }
-    console.log('DB bulundu ve bağlantı sağlandı');
+    //console.log('DB bulundu ve bağlantı sağlandı');
   });
 
   //db oluştur yoksa
@@ -340,28 +413,6 @@ bot.on('presenceUpdate', (oldPresence, newPresence) => {
         timepast = Math.abs(Math.floor((datenow.getTime() - lastsalute.getTime()) / 1000 / 60 / 60));
         //timepast = Math.abs(Math.floor((datenow.getTime() - lastsalute.getTime()) / 1000));
         console.log(`Yeni Statüsü= ${status}\t"Eski Statü= ${oldstatus}\n${id}\t${username}\tson online zamanı = ${lastsalute}\t\t ${row.LastSalute}\nGeçen Süre = ${timepast}`);
-
-        //if (!(status === 'offline') & (g_id === settings.home_office)) {
-        /*
-        if ((g_id === settings.home_office) & (timepast > 4) & (oldstatus === 'offline')) {  
-          switch (id) {
-            case settings.desire: 
-            case settings.efe: 
-            case settings.crop: 
-            case settings.frk: 
-            case settings.just: 
-            case settings.soykan: 
-            case settings.cikko: gunluk.send('SA <@'+id+'> Bro! Hoş geldin :)'); isSaluted = true; break;  
-          }
-        }
-        
-        if (!(status === 'offline') & (timepast > 4) & (g_id === settings.devops)) {
-          switch (id) {
-            case settings.rojeryo: bot.channels.cache.get('722044772750983241').send('SA <@'+id+'> Bro! Hoş geldin :)'); isSaluted = true; break;          
-          }
-        }
-        */
-
         if ((timepast > 4) & (oldstatus === 'offline')) {
           if (g_id === settings.home_office){  
             switch (id) {
@@ -407,8 +458,6 @@ bot.on('presenceUpdate', (oldPresence, newPresence) => {
 
 // Listener Event: Runs whenever the bot sends a ready event (when it first starts for example)
 bot.on('ready', () => {
-
-    // We can post into the console that the bot launched.
     console.log('Bot started.');
 });
 
